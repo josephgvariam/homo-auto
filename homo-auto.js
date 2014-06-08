655b5dc074072a312b9d3163b4443e8c751563a2
Joseph George Variam <joseph@variam.com>
2014-06-08 13:19:05 +0530

    Added Johnny-Five

--- homo-auto.js
+++ homo-auto.js
@@ -20,3 +20,14 @@ var Meal = Backbone.Model.extend({
 });
 
 console.log("Dessert will be " + (new Meal).get('dessert'));
+
+var five = require("./johnny-five/lib/johnny-five"),
+    board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create an Led on pin 13 and strobe it on/off
+  // Optionally set the speed; defaults to 100ms
+  (new five.Led(13)).strobe();
+
+});
